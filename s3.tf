94229ccacff722a10b363878e900fe52fa821eb2
rajesh2285 <mukkapatirajesh9@gmail.com>
2020-03-05 09:19:22 -0500

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-west-1"
+}
+resource "aws_s3_bucket" "b" {
+  bucket = "rajesh-terraform-demo-1"
+  acl    = "private"
+  #region = "us-east-1"
+  tags = {
+    Name        = "rajesh bucket"
+    Environment = "Dev"
+  }
+  versioning {
+    enabled = true
+  }
+}
