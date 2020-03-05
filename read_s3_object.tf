0f91d382e7bcbe28fbd4cef07e223e2049cdb613
rajesh2285 <mukkapatirajesh9@gmail.com>
2020-03-05 16:24:01 -0500

    Update read_s3_object.tf

--- read_s3_object.tf
+++ read_s3_object.tf
@@ -2,7 +2,6 @@ data "aws_s3_bucket_object" "moderate-s3-bucket" {
   bucket = "moderate-s3-bucket"
   key    = "LICENSE.txt"
 }
-
 resource "aws_instance" "example" {
   instance_type = "t2.micro"
   ami           = "ami-002cc39e7bf021a77"
